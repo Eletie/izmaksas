748e3f49a08d9eaffb3bc462cdb13fc19e069ec2
Eletie <60183758+Eletie@users.noreply.github.com>
2023-02-09 20:38:45 +0000

    daudz ko +laiks

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import biblioteka
+import time
 
-print("random suds ja")
+print("Labriit! Esi atvēris mūsu 100% vislabāko darbu, ko vien varējām izdarīt. Šeit saņemsiet visu ko jūs norādījāt specifikācijā <3")
 
 
-class Rekins:
-  def __init__(self, klients, veltijums, izmers, materials):
+class Rekins():
+  def __init__(self, klients, veltijums, izmers, materials, laiks):
     self.klients = klients
     self.veltijums = veltijums
     self.izmers = izmers
     self.materials = materials
-
-  def laiks(self):
-    pass
-
+    self.laiks = laiks
+    
+  def RadaLaiku(self):
+    print("Rēķina ātrums: ", self.laiks)
+    
   def izdrukat(self):
     pass
 
 class Klients1(Rekins):
   def __init__(self):
+    start = time.time()
     while True:
        self.klients = input("Ievadi savu vardu: ")
        if all(x.isalpha() or x.isspace() for x in self.klients):
@@ -32,17 +35,29 @@ class Klients1(Rekins):
        else:
            print("Ievadi veltijumu ar VARDIEM:)<3")    
     try:
-       self.izmers = int(input("Ievadi izmeru(veselu skaitli): "))
+       self.platums = int(input("Ievadi platumu(veselu skaitli): "))
+       self.garmus = int(input("Ievadi garumu(veselu skaitli): "))
+       self.augstums = int(input("Ievadi augstumu(veselu skaitli): "))
     except:
-       print("Ievadi izmēru izmantojot tikai ciparus")
+       print("Ievadi izmēru izmantojot tikai veselus skaitļus")
     
-    while True:
-       self.materials = input("Ievadi izmantojamo materialu: ")
-       if all(x.isalpha() or x.isspace() for x in self.materials):
-           break
-       else:
-           print("Ievadi ar burtiem nevis skaitliem <3") 
+    try:
+       self.materials = int(input("Ievadi izmantojamā kokmateriāla cenu EUR/m^2: "))
+    except:
+       print("Ievadi kokmateriala cenu ar cipariem<3")
+    end = time.time()
+    self.laiks = round(end - start, 2)
 
-Klients1 = Klients1()
 
-#Vieta, kur nolikt kursoru:       
+Klients1 = Klients1() 
+
+Klients1.RadaLaiku()
+
+
+
+
+
+
+#Vieta, kur nolikt kursoru:    
+
+
