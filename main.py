65084f16b4fe88224b976f6aeac597e7b0f71c6d
Eletie <60183758+Eletie@users.noreply.github.com>
2023-02-10 12:39:04 +0000

    produktivi

--- main.py
+++ main.py
@@ -1,32 +1,68 @@
 import biblioteka
-import time
+from datetime import datetime
+
 
 print("Labriit! Esi atvēris 100% vislabāko darbu, ko vien varējām izdarīt. Šeit saņemsiet visu ko jūs norādījāt specifikācijā <3")
 
 
 class Rekins():
-  def __init__(self, klients, veltijums, izmers, materials, laiks):
+  def __init__(self, klients, veltijums, izmers, materials, laiks, aprekins):
     self.klients = klients
     self.veltijums = veltijums
     self.izmers = izmers
     self.materials = materials
     self.laiks = laiks
+    self.aprekins = aprekins
     
   def RadaLaiku(self):
-    print("Rēķina ātrums:", self.laiks)
+    currentDateAndTime = datetime.now()
+    print("The current date and time is", currentDateAndTime)
+
+    currentTime = currentDateAndTime.strftime("%H:%M:%S")
+    print("The current time is", currentTime)
     
   def izdrukat(self):
-    print("Rēķina info:\n","Vārds:", self.klients)#un tā turpinat talak ez es gulet.
+    print("Rēķina info:\n","Vārds:", self.klients)
+    print(" Veltījums:", self.veltijums)
+    print(" Platums:", self.platums)
+    print(" Garums:", self.garums)
+    print(" Augstums:", self.augstums)
+    print(" Materiāla cena:", self.materials)
+    print(" Produkta cena:", self.aprekins)
+    
+    #un tā turpinat talak ez es gulet.
 
   def aprekins(self):
-    pass #šeit apreķins pec formulas kaut kā(ok lab šo gruti saprast, kas ir metode. kada atšķiriba ipašiba vai metode)
+    darba_samaksa = 15
+    PVN = 21
+    teksta_garums = len(self.veltijums)
+    #print("produkta cena: ", teksta_garums)
+    p = int(self.platums)
+    g = int(self.garums)
+    a = int(self.augstums)
+    m = int(self.materials)
+
+    produkta_cena = int(teksta_garums) * 1.2 + (p/100 * g/100 * 
+    a/100)/ 3 * m
+
+    #print("produkta cena: ", produkta_cena)
+
+    PVN_summa = int((produkta_cena + darba_samaksa)*PVN/100)
+
+    rekina_summa = int((produkta_cena + darba_samaksa) + PVN_summa)
+
+    self.aprekins = rekina_summa
+    #print("produkta cena: ", round(produkta_cena, 2))
+    #print("pvnsumma: ", PVN_summa)
+    #print("Rēķina summa: ", rekina_summa)
+
+    
   def saglabat(self):
     pass#saglaba visu csv, kas atrodas šaja class...
 
 
 class Klients1(Rekins):
   def __init__(self):
-    start = time.time()
     while True:
        self.klients = input("Ievadi savu vardu: ")
        if all(x.isalpha() or x.isspace() for x in self.klients):
@@ -36,25 +72,44 @@ class Klients1(Rekins):
     
     while True:
        self.veltijums = input("Ievadi veltijumu: ")
-       if all(x.isalpha() or x.isspace() for x in self.veltijums):
+       if all(x.isalpha() or x.isspace() or x.isnumeric() for x in self.veltijums):
            break
        else:
            print("Ievadi veltijumu ar VARDIEM:)<3")  
-         
-    try:
-      self.platums = int(input("Ievadi platumu(veselu skaitli): "))
-      self.garmus = int(input("Ievadi garumu(veselu skaitli): "))
-      self.augstums = int(input("Ievadi augstumu(veselu skaitli): "))
-    except:
-         print("Ievadi izmēru izmantojot tikai veselus skaitļus")
+
+    while True:
+      self.garums = input("Ievadi garumu(veselu skaitli): ")  
+      if all(x.isnumeric() for x in self.garums):
+        break 
+      else:
+        print("Ievadi izmēru izmantojot tikai veselus skaitļus")
+
+    while True:
+      self.platums = input("Ievadi platumu(veselu skaitli): ")    
+      if all(x.isnumeric() for x in self.platums):
+        break 
+      else:
+        print("Ievadi izmēru izmantojot tikai veselus skaitļus")
+
+    while True:
+      self.augstums = input("Ievadi augstumu(veselu skaitli): ")    
+      if all(x.isnumeric() for x in self.augstums):
+        break 
+      else:
+        print("Ievadi izmēru izmantojot tikai veselus skaitļus")
+
+        
+    while True:
+      self.materials = (input("Ievadi izmantojamā kokmateriāla cenu EUR/m^2: ")) 
+      if all(x.isnumeric() for x in self.materials):
+        break 
+      else:
+        print("Ievadi kokmateriala cenu ar cipariem<3")    
+
     
-    try:
-       self.materials = int(input("Ievadi izmantojamā kokmateriāla cenu EUR/m^2: "))
-    except:
-       print("Ievadi kokmateriala cenu ar cipariem<3")
-      
-    end = time.time()
-    self.laiks = round(end - start, 2) #šeit ir problēmas, mana problema ka laiku jāciklē, jo jā un tagad ievadīšanā problēma.jo kad ievadu nepareizi platumu viss man prasa uzreiz materialus, nav labi.
+    
+
+     #šeit ir problēmas, mana problema ka laiku jāciklē, jo jā un tagad ievadīšanā problēma.jo kad ievadu nepareizi platumu viss man prasa uzreiz materialus, nav labi.
 
 
   
@@ -64,8 +119,9 @@ Klients1 = Klients1()
 
 Klients1.RadaLaiku()
 
-Klients1.izdrukat()
+Klients1.aprekins()
 
+Klients1.izdrukat()
 
 
 
@@ -73,3 +129,4 @@ Klients1.izdrukat()
 #Vieta, kur nolikt kursoru:    
 
 
+#es nezino ko darīt, sorry :(   #depresija #dead_inside #dumb_men
